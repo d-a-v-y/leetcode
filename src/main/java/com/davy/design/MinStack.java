c371e827ebe8aea79928f5cb8c595699ef9c463c
Davy <2689022556@qq.com>
2022-05-20 09:13:34 +0800

    2022_5_20 commit

---
+++ src/main/java/com/davy/design/MinStack.java
@@ -0,0 +1,37 @@
+package com.davy.design;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+/**
+ * @author Davy
+ */
+public class MinStack {
+
+    Deque<Integer> xStack;
+    Deque<Integer> minStack;
+
+    public MinStack() {
+        xStack = new LinkedList<Integer>();
+        minStack = new LinkedList<Integer>();
+        minStack.push(Integer.MAX_VALUE);
+    }
+
+    public void push(int val) {
+        xStack.push(val);
+        minStack.push(Math.min(minStack.peek(),val));
+    }
+
+    public void pop() {
+        xStack.pop();
+        minStack.pop();
+    }
+
+    public int top() {
+        return xStack.peek();
+    }
+
+    public int getMin() {
+        return minStack.peek();
+    }
+}
